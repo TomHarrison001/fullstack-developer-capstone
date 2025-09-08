c3ffb6c85b9fdfd9005cbc73a05c9c64c4df6666
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-09-08 13:57:48 +0100

    Populate car makes and models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://tomharrison0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://tomharrison0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://tomharrison0-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://tomharrison0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://tomharrison0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://tomharrison0-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
