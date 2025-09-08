cf729c05158c35a0af13b72e36f72c82cac0a565
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-09-08 16:06:42 +0100

    Check python linter

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -21,8 +21,7 @@ class CarModel(models.Model):
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2025, validators=[
         MaxValueValidator(2025),
-        MinValueValidator(2015)
-    ])
-    
+        MinValueValidator(2015)])
+
     def __str__(self):
         return self.name
