c433800e5bd76217c6ce4590850d7aa8a15b8650
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2025-09-08 16:43:50 +0100

    Complete python linting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://tomharrison0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://tomharrison0-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://tomharrison0-8000.theianext-1-labs-\
+        prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://tomharrison0-8000.theiadockernext-1-labs\
+        -prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://tomharrison0-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://tomharrison0-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://tomharrison0-8000.theianext-1-labs\
+        -prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://tomharrison0-8000.theiadockernext-1-labs\
+        -prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -98,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation\
+            .UserAttributeSimilarityValidator',
     },
     {
         'NAME':
